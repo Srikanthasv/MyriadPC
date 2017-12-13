a427e20d1c89b3996152e63de1c0c4f8ede7301c
Srikantha <srikanthasv@gmail.com>
2017-12-13 10:31:09 +0530

    code modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',  './bootstrap.css']
 })
 export class AppComponent {
   title = 'app';
+  
+//  public urlForm: FormGroup;
+//  username = new FormControl('', [Validators.required]);
+//password = new FormControl('', [Validators.required]);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,108 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { routing } from './app.routes';  
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { LoginService } from './_services/login.service';
+import { enableProdMode } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+enableProdMode();
+
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,MatFormFieldModule, 
+} from '@angular/material';
+import {CdkTableModule} from '@angular/cdk/table';
 
 @NgModule({
+	exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,MatFormFieldModule,MatStepperModule,
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ReactiveFormsModule,HttpModule ,routing,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
+    JsonpModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,MatFormFieldModule,MatInputModule,BrowserAnimationsModule,MatIconModule,MatTableModule,MatButtonModule,MatMenuModule,MatSelectModule,
+	MatTabsModule ],
+  providers: [LoginService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
