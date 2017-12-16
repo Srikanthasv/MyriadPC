3033acbde853ceaebcf2e53359cbf1c39e6a4ed0
kishore <madishetty.kishore@gmail.com>
2017-12-17 00:17:43 +0530

    code added for sorting,add code using material and validations,edit code page , populated the code list using json array

--- src/app/Configuration/Codes/advertising/advertising.component.ts
+++ src/app/Configuration/Codes/advertising/advertising.component.ts
@@ -1,18 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Pipe, PipeTransform } from '@angular/core';
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-advertising',
  // templateUrl: './edit.component.html',
-  templateUrl: './Add.component.html',
-  //templateUrl: './advertising.component.html',
+  //templateUrl: './Add.component.html',
+  templateUrl: './advertising.component.html',
   styleUrls: ['./advertising.component.css', '../../../bootstrap.css']
 })
 export class AdvertisingComponent implements OnInit {
 
-  constructor() { }
+codesList:any;
+  public codeForm: FormGroup;
+addFlag:boolean=false;
+displayFlag:boolean=true;
+sortby:any;
+sortorder:any;
+code:any;
+description:any;
+//searchbox:boolean=true;
+  constructor(private fb: FormBuilder) { 
+this.codesList={ 'Data': [ { 'Id': 1, 'Code': 'B', 'Description': 'BILLBOARD', 'StartDate': '2014-07-20T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEBM=' }, { 'Id': 2, 'Code': 'CM', 'Description': 'CUSTOMER MAILER', 'StartDate': '2014-07-20T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEBI=' }, { 'Id': 3, 'Code': 'CPN', 'Description': 'Coupon', 'StartDate': '2014-10-14T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEBE=' }, { 'Id': 4, 'Code': 'EML', 'Description': 'Email', 'StartDate': '2014-10-14T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEBA=' }, { 'Id': 5, 'Code': 'FB', 'Description': 'FaceBook', 'StartDate': '2014-09-17T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEA8=' }, { 'Id': 6, 'Code': 'INT', 'Description': 'Interior Design spec magazine', 'StartDate': '2014-10-14T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEA4=' }, { 'Id': 7, 'Code': 'N', 'Description': 'NEWSPAPER', 'StartDate': '2014-07-20T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEA0=' }, { 'Id': 8, 'Code': 'NP', 'Description': 'NON PROFIT - MUST HAVE NP ID #', 'StartDate': '2014-07-20T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEAw=' }, { 'Id': 9, 'Code': 'PIN', 'Description': 'Pinterest', 'StartDate': '2014-09-17T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEAs=' }, { 'Id': 10, 'Code': 'R', 'Description': 'RADIO', 'StartDate': '2014-07-20T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEAo=' }, { 'Id': 11, 'Code': 'RP', 'Description': 'REPEAT CUSTOMER', 'StartDate': '2014-07-20T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEAk=' }, { 'Id': 12, 'Code': 'W', 'Description': 'WORD OF MOUTH', 'StartDate': '2014-07-20T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEAg=' }, { 'Id': 13, 'Code': 'TWEET', 'Description': 'Twitter', 'StartDate': '2016-12-13T00:00:00', 'EndDate': null, 'TimeStamp': 'AAAAAAAYEAc=' } ], 'Total': 13, 'AggregateResults': null, 'Errors': null } ;
 
-  ngOnInit() {
+  this.sortby="Code";
+	this.sortorder="asc";
   }
 
+  ngOnInit() {
+
+  this.codeForm = this.fb.group({
+      'code' : [null, Validators.compose([Validators.required])],
+      'description' : [null, Validators.compose([Validators.required])],
+      // Validators.pattern('[a-zA-Z ]*')
+    });
+  }
+  callcodes(){
+  	console.log("Codes entered");
+  	this.addFlag=true;
+  	this.displayFlag=false;
+  	//this.code="";
+	//this.description="";
+  }
+  savecode(value){
+  	if (this.codeForm.valid)
+    {
+  	this.addFlag=false;
+  	this.displayFlag=true;
+  }
 }
+   editcodes(id,code,desc){
+  	this.addFlag=true;
+  	this.displayFlag=false;
+  	this.code=code;
+  	this.description=desc;
+  }
+
+  clearsort(){
+	this.sortby="Code";
+	this.sortorder="asc";
+  }
+  clearaddcodes(){
+	this.code="";
+	this.description="";
+  }
+  filter(){
+  	  	//  this.searchbox=true;
+
+  }
+  sortcodes(){
+  	let  orderType=this.sortorder
+  	let currentField=this.sortby;
+  	 // this.searchbox=false;
+
+    this.codesList.Data.sort((a: any, b: any) => {
+    	                if (orderType === 'asc') {
+                    if (a[currentField] < b[currentField]) return -1;
+                    if (a[currentField] > b[currentField]) return 1;
+                    return 0;
+                } else {
+                    if (a[currentField] < b[currentField]) return 1;
+                    if (a[currentField] > b[currentField]) return -1;
+                    return 0;
+                }
+
+    });
+    return this.codesList.Data;
+  }
+}
+
