dc2d6355f969be8a453330dd9f3106d98e1008f2
Srikantha <srikanthasv@gmail.com>
2018-01-03 14:24:44 +0530

    Modelcost

--- src/app/Configuration/Codes/modelcost/modelcost.component.ts
+++ src/app/Configuration/Codes/modelcost/modelcost.component.ts
@@ -1,15 +1,264 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, AfterContentInit, AfterViewInit, Renderer, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Router } from '@angular/router';
+import { Headers, Http, RequestOptions, Response } from '@angular/http';
+import { MatPaginator, MatSort, MatTableDataSource, MatPaginatorModule } from '@angular/material';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { ModelcostService } from '../../../_services/modelcost.service';
 
 @Component({
   selector: 'app-modelcost',
   templateUrl: './modelcost.component.html',
-  styleUrls: ['../../../../assets/css/configuration/configuration.css']
+  styleUrls: ['../../../../assets/css/configuration/configuration.css'],
+  providers: [ModelcostService]
 })
 export class ModelcostComponent implements OnInit {
 
-  constructor() { }
+  public codesList: any = "";
+  public codeForm: FormGroup;
+  codeeditForm: FormGroup;
+  sortby: any;
+  sortorder: any;
+  data: any[];
+  errorMsg: string = "";
+  addFlag: boolean = false;
+  displayFlag: boolean = true;
+  code: any;
+  description: any;
+  editFlag: boolean = false;
+  effectiveDate: any;
+  discontinuedDate: any;
+
+  editdescription: any;
+  prevDescription: any;
+  prevDiscDate: any;
+  editcode: any;
+  id: any;
+  codeerrorMsg: string = '';
+  descerrorMsg: string = '';
+  editcodeerrorMsg: string = '';
+  editdescerrorMsg: string = '';
+  errormsg: string;
+  bReturn: boolean = false;
+  editerrormsg: string = "";
+  pgTitle: string = "Model Cost";
+  minDate: Date;
+
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private http: Http,
+    private ModelcostService: ModelcostService) {
+    this.sortby = "Code";
+    this.sortorder = "asc";
+    this.getData();
+  }
+  displayedColumns = ['Id', 'Code', 'Description', 'StartDate', 'EndDate', 'TimeStamp'];
+  dataSource: MatTableDataSource<ModelcostData>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit() {
+    this.codeForm = this.fb.group({
+      'code': [null, Validators.compose([Validators.required])],
+      'description': [null, Validators.compose([Validators.required])]
+    });
+    this.codeeditForm = this.fb.group({
+      'editdescription': [null, Validators.compose([Validators.required])],
+      // 'discontinuedDate': [null, Validators.compose([Validators.required])],
+      // Validators.pattern('[a-zA-Z ]*')
+    });
+  }
+  getData() {
+
+    this.ModelcostService.getMCData()
+      .subscribe(res => {
+        this.codesList = res;
+        this.data = this.codesList.Data;
+        this.dataSource = new MatTableDataSource(this.data);
+        this.dataSource.paginator = this.paginator;
+        this.sortMCcodes();
+      },
+      err => {
+        this.errorMsg = "Model Cost codes not found.";
+        console.log("error:" + err);
+      }
+      );
+  }
+  callMCCodes() {
+    this.addFlag = true;
+    this.displayFlag = false;
+    this.errormsg = "";
+    this.codeerrorMsg = "";
+    this.descerrorMsg = "";
+    this.code = "";
+    this.description = "";
+    this.pgTitle = "Add Model Cost Code";
+  }
+  addMCcodeback() {
+    this.addFlag = false;
+    this.displayFlag = true;
+    this.code = "";
+    this.description = "";
+    this.getData();
+    this.pgTitle = "Model Cost";
+  }
+  clearMCaddcodes() {
+    this.code = "";
+    this.description = "";
+    this.errormsg = "";
+    this.codeerrorMsg = "";
+    this.descerrorMsg = ""
   }
 
+  saveMCcode(value) {
+    this.errormsg = "";
+    this.codeerrorMsg = "";
+    this.descerrorMsg = "";
+    if (this.codeForm.valid) {
+      if ((this.code || '').trim().length == 0) {
+        this.codeerrorMsg = "Please enter valid input for Code.";
+        return false;
+      }
+      if ((this.description || '').trim().length == 0) {
+        this.descerrorMsg = "Please enter valid input for Description."
+        return false;
+      }
+      var body = JSON.stringify({
+        "Code": this.code,
+        "Description": this.description,
+        "StartDate": new Date(),
+        "EndDate": null
+      });
+
+      this.ModelcostService.submitMC(body)
+        .subscribe(res => {
+          this.addFlag = false;
+          this.codesList = "";
+          this.displayFlag = true;
+          this.getData();
+          this.pgTitle = "Model Cost";
+        },
+        err => {
+          err = err
+          console.log("Error" + err);
+          this.errormsg = "Failed to save the code details.";
+        }
+        );
+    }
+  }
+
+  editMCcodes(id, code, desc, startDate, endDate, isSystem) {
+    this.editFlag = true;
+    this.displayFlag = false;
+    this.editcode = code;
+    this.editdescription = desc;
+    this.prevDescription = desc;
+    var splitted = startDate.split("T", 2);
+    this.effectiveDate = splitted[0];
+    if (endDate || '') {
+      splitted = '';
+      splitted = endDate.split("T", 2);;
+      this.discontinuedDate = splitted[0];
+    }
+    this.id = id;
+    this.prevDiscDate = endDate;
+    this.editerrormsg = "";
+    this.editcodeerrorMsg = "";
+    this.editdescerrorMsg = "";
+    this.pgTitle = "Edit Model Cost Code";
+  }
+
+  updateMCeditcode(code, value) {
+    this.editerrormsg = "";
+    if (this.codeeditForm.valid) {
+      if ((this.editcode || '').trim().length === 0) {
+        this.editcodeerrorMsg = "Please enter valid input for Code."
+        return false;
+      }
+      if ((this.editdescription || '').trim().length === 0) {
+        this.editdescerrorMsg = "Please enter valid input for Description."
+        return false;
+      }
+
+      this.editFlag = false;
+      this.displayFlag = true;
+
+      var body = JSON.stringify({
+        "Id": this.id,
+        "Code": this.editcode,
+        "Description": this.editdescription,
+        "StartDate": this.effectiveDate,
+        "EndDate": this.discontinuedDate
+      });
+      this.ModelcostService.updateMC(this.id, body)
+        .subscribe(res => {
+          this.editFlag = false;
+          this.codesList = "";
+          this.displayFlag = true;
+          this.getData();
+          this.id = "";
+          this.pgTitle = "Model Cost";
+        },
+        err => {
+          err = err
+          console.log(err);
+          this.editerrormsg = "Failed to update the code details.";
+        }
+        );
+    }
+  }
+  editMCcodeback() {
+    this.editFlag = false;
+    this.displayFlag = true;
+    this.editcode = "";
+    this.editdescription = "";
+    this.getData();
+    this.pgTitle = "Model Cost";
+    this.id = "";
+  }
+  clearMCsort() {
+    this.sortby = "Code";
+    this.sortorder = "asc";
+  }
+
+  clearMCeditcodes() {
+    this.editdescription = this.prevDescription;
+    this.discontinuedDate = this.prevDiscDate;
+    this.editerrormsg = "";
+    this.editcodeerrorMsg = "";
+    this.editdescerrorMsg = "";
+  }
+
+  sortMCcodes() {
+    let orderType = this.sortorder
+    let currentField = this.sortby;
+    this.codesList.Data.sort((a: any, b: any) => {
+      if (orderType === 'asc') {
+        if (a[currentField] < b[currentField]) return -1;
+        if (a[currentField] > b[currentField]) return 1;
+        return 0;
+      } else {
+        if (a[currentField] < b[currentField]) return 1;
+        if (a[currentField] > b[currentField]) return -1;
+        return 0;
+      }
+
+    });
+    this.data = this.codesList.Data;
+    this.dataSource = new MatTableDataSource(this.data);
+    this.dataSource.paginator = this.paginator;
+    return this.codesList.Data;
+  }
+
+}
+
+export interface ModelcostData {
+  Id: any;
+  Code: any;
+  StartDate: any;
+  EndDate: any;
+  Description: any;
+  TimeStamp: any;
 }
